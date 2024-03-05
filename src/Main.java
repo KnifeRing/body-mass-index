936133a4fc205f885b867093427ec668dd3e28a0
Alexander Khyrbu <rjrednaxela@vk.ru>
2024-03-06 00:18:03 +0300

    ed logic

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double weight, height;
-        weight = 30; // вес в кг
-        height = 1.5; // рост в метрах
+        weight = 60; // вес в кг
+        height = 1.87; // рост в метрах
         String result = null;
         result = BmiService.calculate(weight, height);
         System.out.println(result);
